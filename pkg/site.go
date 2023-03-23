76de2df96ac9a1d11c508cdbee72fddfd82dcb8b
John Mulhern <jmm@hey.com>
2023-03-22 20:38:22 -0700

    0.0.7 - added description to site

--- pkg/site.go
+++ pkg/site.go
@@ -2,6 +2,7 @@ package pkg
 
 type Site struct {
 	Title       string `json:"title,omitempty" yaml:"title,omitempty"`
+	Description string `json:"description,omitempty" yaml:"description,omitempty"`
 	Favicon     string `json:"favicon,omitempty" yaml:"favicon,omitempty"`
 	BodyClasses string `json:"body_classes,omitempty" yaml:"body-classes,omitempty"`
 	Email       Email  `json:"email,omitempty" yaml:"email,omitempty"`
