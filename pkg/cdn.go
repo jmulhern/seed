5ead41baa3ac6fed6f377eb87907467e41d54cfd
John Mulhern <jmm@hey.com>
2023-03-26 15:06:32 -0700

    0.2.1 - nothing important

--- pkg/cdn.go
+++ pkg/cdn.go
@@ -1,6 +1,6 @@
 package pkg
 
 type CDN struct {
-	FQDM   string `json:"fqdn,omitempty" yaml:"fqdn,omitempty"`
+	FQDN   string `json:"fqdn,omitempty" yaml:"fqdn,omitempty"`
 	Bucket string `json:"bucket,omitempty" yaml:"bucket,omitempty"`
 }
