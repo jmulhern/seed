4fa3707a394c894a37dfdbd9aa7ee140afa4932d
John Mulhern <jmm@hey.com>
2023-07-19 21:48:01 -0700

    0.4.3 - stripe

--- pkg/stripe.go
+++ pkg/stripe.go
@@ -4,6 +4,4 @@ type Stripe struct {
 	Enabled        bool   `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	PublishableKey string `json:"publishable_key,omitempty" yaml:"publishable-key,omitempty"`
 	SecretKey      string `json:"secret_key,omitempty" yaml:"secret-key,omitempty"`
-	BasicPriceID   string `json:"basic_price_id,omitempty" yaml:"basic-price-id,omitempty"`
-	ProPriceID     string `json:"pro_price_id:,omitempty" yaml:"pro-price-id:,omitempty"`
 }
