ef44999466a324528db75b1f50800c0285fd285c
John Mulhern <jmm@hey.com>
2023-08-19 14:40:04 -0700

    0.5.0 - stripe restructure

--- pkg/checkout.go
+++ pkg/checkout.go
@@ -1,6 +1,5 @@
 package heritage
 
 type Checkout struct {
-	SuccessURL string `json:"success_url,omitempty" yaml:"success-url,omitempty"`
-	CancelURL  string `json:"cancel_url,omitempty" yaml:"cancel-url,omitempty"`
+	Stripe Stripe `json:"stripe" yaml:"stripe"`
 }
--- pkg/stripe.go
+++ pkg/stripe.go
@@ -1,7 +1,8 @@
 package heritage
 
 type Stripe struct {
-	Enabled        bool   `json:"enabled,omitempty" yaml:"enabled,omitempty"`
-	PublishableKey string `json:"publishable_key,omitempty" yaml:"publishable-key,omitempty"`
-	SecretKey      string `json:"secret_key,omitempty" yaml:"secret-key,omitempty"`
+	Enabled    bool   `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	SecretKey  string `json:"secret_key,omitempty" yaml:"secret-key,omitempty"`
+	SuccessURL string `json:"success_url,omitempty" yaml:"success-url,omitempty"`
+	CancelURL  string `json:"cancel_url,omitempty" yaml:"cancel-url,omitempty"`
 }
